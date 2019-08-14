3a72b3d647f145c5060e5a3d5dec25588528590f
deadpool <deadpool@KevinFlynn.mshome.net>
2019-08-14 17:54:21 -0500

    Initial Commit

---
+++ BasicJavaFXOne/src/jansproj/basicfx/BasicJavaFXOne.java
@@ -0,0 +1,29 @@
+package jansproj.basicfx;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class BasicJavaFXOne extends Application 
+{
+
+	@Override
+	public void start(Stage stage) throws Exception 
+	{
+        Circle circ = new Circle(40, 40, 30);
+        Group root = new Group(circ);
+        Scene scene = new Scene(root, 400, 300);
+
+        stage.setTitle("My JavaFX Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+	public static void main(String[] args) 
+	{
+		Application.launch(args);
+	}
+
+}
